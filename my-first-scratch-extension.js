99124f6c999011cf67221c43060a3a638c9ccf09
Joe Lee <joe@Joes-MacBook-Pro.local>
2020-03-10 22:36:08 +0800

    changed js

--- my-first-scratch-extension.js
+++ my-first-scratch-extension.js
@@ -1,4 +1,10 @@
 (function(ext) {
+
+	$.getScript('https://scl1003.github.io/test-scratch-block/aws-sdk-2.636.0.js', initExtension);
+
+	var accessKeyId = '';
+	var secretAccessKey = '';
+
     // Cleanup function when the extension is unloaded
     ext._shutdown = function() {};
 
@@ -12,10 +18,28 @@
         // Code that gets executed when the block is run
     };
 
+    function initAWS(region) {
+		AWS.config.region = region;
+		AWS.config.accessKeyId = accessKeyId;
+		AWS.config.secretAccessKey = secretAccessKey;
+	}
+
+	// Initialization services
+	
+	ext.initAWSServices = function(region) {
+		if (accessKeyId === '')
+			accessKeyId = prompt('Please enter your AWS_ACCESS_KEY_ID');
+		if (secretAccessKey === '')
+			secretAccessKey = prompt('Please enter your AWS_SECRET_ACCESS_KEY');
+		
+		initAWSServices(region);
+	}
+
     // Block and block menu descriptions
     var descriptor = {
         blocks: [
             // Block type, block name, function name
+            [' ', 'init AWS %s', 'initAWSServices', 'eu-west-1'],   
             [' ', 'my first block', 'my_first_block'],
         ]
     };
